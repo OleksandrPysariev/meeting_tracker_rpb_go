e7176a49e7d92496dbd7189055b09058556b60b4
OP <apalexlife@gmail.com>
2024-06-13 09:54:20 +0300

    --first attempt at implementing i2c lcd screen in go;

--- src/meeting_tracker_rpb_go/go.mod
+++ src/meeting_tracker_rpb_go/go.mod
@@ -1,3 +1,11 @@
 module github.com/OleksandrPysariev/meeting_tracker_rpb_go
 
 go 1.19
+
+require (
+	github.com/d2r2/go-hd44780 v0.0.0-20181002113701-74cc28c83a3e // indirect
+	github.com/d2r2/go-i2c v0.0.0-20191123181816-73a8a799d6bc // indirect
+	github.com/d2r2/go-logger v0.0.0-20210606094344-60e9d1233e22 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
+)
--- src/meeting_tracker_rpb_go/read_event/read_event.go
+++ src/meeting_tracker_rpb_go/read_event/read_event.go
@@ -3,6 +3,10 @@ package read_event
 import (
 	"fmt"
 	"time"
+	"log"
+
+	device "github.com/d2r2/go-hd44780"
+	"github.com/d2r2/go-i2c"
 
 	"github.com/OleksandrPysariev/meeting_tracker_rpb_go/read_event/utils"
 )
@@ -10,8 +14,41 @@ import (
 var BACKLIGHT = true
 var ON = true
 
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func switch_backlight(lcd device.Lcd){
+	BACKLIGHT = !BACKLIGHT
+	if BACKLIGHT {
+		err := lcd.BacklightOn()
+		checkError(err)
+	} else {
+		err := lcd.BacklightOff()
+		checkError(err)
+	}
+}
+
+func switch_on_off(lcd device.Lcd){
+    ON = !ON
+    switch_backlight(lcd)
+    lcd.Clear()
+}
+
 func RunReadEvent() {
 	fmt.Print("Running RunReadEvent...")
+	
+	// Connect to i2c bus
+	i2c, err := i2c.NewI2C(0x27, 2)
+	checkError(err)
+	defer i2c.Close()
+	// Instantiate lcd screen
+	lcd, err := device.NewLcd(i2c, device.LCD_16x2)
+	checkError(err)
+
+
 	currentlyInTheMeeting := false
 	event := read_event.ParseEvent()
 	lastCalled := read_event.TimeNow()
@@ -43,13 +80,22 @@ func RunReadEvent() {
 			line1 += "    "
 
 			line2 := ""
-			read_event.Say(line1, line2)
+			// read_event.Say(line1, line2)
+			err = lcd.ShowMessage(line1, device.SHOW_LINE_1)
+			checkError(err)
+			err = lcd.ShowMessage(line2, device.SHOW_LINE_2)
+			checkError(err)
 		} else {
 			line1 := read_event.TimeNow().Format("15:04:05")
 			line1 += " "
 			line1 += event.Time
 			line2 := event.Description
-			read_event.Say(line1, line2)
+			// read_event.Say(line1, line2)
+			err = lcd.ShowMessage(line1, device.SHOW_LINE_1)
+			checkError(err)
+			err = lcd.ShowMessage(line2, device.SHOW_LINE_2)
+			checkError(err)
+
 		}
 		time.Sleep(10 * time.Millisecond)
 	}
