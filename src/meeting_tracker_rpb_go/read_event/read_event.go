deeec1e1d203fbc54bd208091870c275b0795b30
OP <apalexlife@gmail.com>
2024-06-17 10:45:53 +0300

    -- add button support;

--- src/meeting_tracker_rpb_go/read_event/read_event.go
+++ src/meeting_tracker_rpb_go/read_event/read_event.go
@@ -2,14 +2,14 @@ package read_event
 
 import (
 	"fmt"
-	"log"
 	"time"
 
 	device "github.com/d2r2/go-hd44780"
 	"github.com/d2r2/go-i2c"
 	"github.com/d2r2/go-logger"
+	"github.com/stianeikeland/go-rpio/v4"
 
-	"github.com/OleksandrPysariev/meeting_tracker_rpb_go/read_event/utils"
+	utils "github.com/OleksandrPysariev/meeting_tracker_rpb_go/read_event/utils"
 )
 
 // You can manage verbosity of log output
@@ -20,55 +20,69 @@ var lg = logger.NewPackageLogger("read_event",
 	logger.InfoLevel,
 )
 
+// button = Button(21)
+// button.when_pressed = lambda: switch_on_off(lcd)
+
+var (
+	// Use mcu pin 40, corresponds to GPIO 21 on the pi
+	pin = rpio.Pin(40)
+)
 var BACKLIGHT = true
 var ON = true
-
-func checkError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 func switch_backlight(lcd device.Lcd) {
 	BACKLIGHT = !BACKLIGHT
 	if BACKLIGHT {
 		err := lcd.BacklightOn()
-		checkError(err)
+		utils.CheckError(err)
 	} else {
 		err := lcd.BacklightOff()
-		checkError(err)
+		utils.CheckError(err)
 	}
 }
 
 func switch_on_off(lcd device.Lcd) {
 	ON = !ON
-	switch_backlight(lcd)
 	lcd.Clear()
+	switch_backlight(lcd)
 }
 
 func RunReadEvent() {
 	fmt.Print("[read_event] Running RunReadEvent...\n")
 
+	// Open and map memory to access gpio, check for errors
+	err := rpio.Open()
+	utils.CheckError(err)
+	// Unmap gpio memory when done
+	defer rpio.Close()
+	pin.Input()
+	pin.PullUp()
+	pin.Detect(rpio.FallEdge) // enable falling edge event detection
+
 	// Connect to i2c bus
 	i2c, err := i2c.NewI2C(0x27, 1)
-	checkError(err)
+	utils.CheckError(err)
 	defer i2c.Close()
+
 	// Instantiate lcd screen
 	lcd, err := device.NewLcd(i2c, device.LCD_16x2)
-	checkError(err)
+	utils.CheckError(err)
 	// Enable screen backlight
 	err = lcd.BacklightOn()
-	checkError(err)
+	utils.CheckError(err)
 
 	currentlyInTheMeeting := false
-	event := read_event.ParseEvent()
-	lastCalled := read_event.TimeNow()
+	event := utils.ParseEvent()
+	lastCalled := utils.TimeNow()
 	for {
+		if pin.EdgeDetected() { // check if event occured
+			switch_on_off(*lcd)
+		}
 		if !ON {
 			time.Sleep(1 * time.Second)
 			continue
 		}
-		now := read_event.TimeNow()
+		now := utils.TimeNow()
 		// Monitor when the meeting starts to get the new meeting when it ends
 		if !currentlyInTheMeeting && event.Start.Before(now) && event.End.After(now) {
 			currentlyInTheMeeting = true
@@ -77,40 +91,37 @@ func RunReadEvent() {
 		// Get new meeting to show if you just finished a meeting
 		if currentlyInTheMeeting && event.End.Before(now) {
 			currentlyInTheMeeting = false
-			event = read_event.ParseEvent()
-			lastCalled = read_event.TimeNow()
+			event = utils.ParseEvent()
+			lastCalled = utils.TimeNow()
 		}
 		// Refresh current meeting every 2 minutes to track new meetings throughout the day
 		if now.Sub(lastCalled).Seconds() > 120 {
-			event = read_event.ParseEvent()
-			lastCalled = read_event.TimeNow()
+			event = utils.ParseEvent()
+			lastCalled = utils.TimeNow()
 		}
-		if !read_event.DateEqual(event.Start, read_event.TimeNow()) {
+		if !utils.DateEqual(event.Start, utils.TimeNow()) {
 			line1 := "    "
-			line1 += read_event.TimeNow().Format("15:04:05")
+			line1 += utils.TimeNow().Format("15:04:05")
 			line1 += "    "
 			// lcd can't show empty message so " " is showed
 			line2 := " "
 			err = lcd.ShowMessage(line1, device.SHOW_LINE_1)
-			checkError(err)
+			utils.CheckError(err)
 			err = lcd.ShowMessage(line2, device.SHOW_LINE_2)
-			checkError(err)
+			utils.CheckError(err)
 		} else {
-			line1 := read_event.TimeNow().Format("15:04:05")
+			line1 := utils.TimeNow().Format("15:04:05")
 			// lcd can't show empty message so " " is showed
 			line1 += " "
 			line1 += event.Time
 			line2 := event.Description
 			// read_event.Say(line1, line2)
 			err = lcd.ShowMessage(line1, device.SHOW_LINE_1)
-			checkError(err)
+			utils.CheckError(err)
 			err = lcd.ShowMessage(line2, device.SHOW_LINE_2)
-			checkError(err)
+			utils.CheckError(err)
 
 		}
 		time.Sleep(10 * time.Millisecond)
 	}
-	// TODO: implement LCD commands
-	// https://github.com/d2r2/go-hd44780
-	// fmt.Printf("Received a message: %#v", event)
 }
